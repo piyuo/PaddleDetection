6c6a969a95e33887eb3bf586bced36285bbc4546
piyuo <piyuo.com@gmail.com>
2025-09-27 13:38:32 -0700

    refactor: Remove obsolete ONNX quantization scripts and add new olive configuration for improved model quantization

--- pipeline/PP-YOLOE/onnx_olive.sh
+++ pipeline/PP-YOLOE/onnx_olive.sh
@@ -1,4 +1,4 @@
-# pipeline/PP-YOLOE/onnx_quantize.sh
+# pipeline/PP-YOLOE/onnx_olive.sh
 
 #!/bin/bash
 
@@ -8,7 +8,7 @@ source pipeline/PP-YOLOE/venv/bin/activate
 
 
 # use olive to quantize the onnx model
-olive  run --config pipeline/PP-YOLOE/onnx_quantize.json
+olive  run --config pipeline/PP-YOLOE/onnx_olive.json
 
 # Rename the output model to a more descriptive name
 mv -f pipeline/PP-YOLOE/models/olive/model.onnx pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_embed_olive.onnx
