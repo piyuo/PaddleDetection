187e691252ac3ad2c60bce75957bf84da5995aca
piyuo <piyuo.com@gmail.com>
2025-09-28 14:03:30 -0700

    fix: Update warmup and runs parameters in graph surgery comparison script

--- pipeline/PP-YOLOE/graph_surgery_compare.sh
+++ pipeline/PP-YOLOE/graph_surgery_compare.sh
@@ -3,7 +3,7 @@
 
 python3 pipeline/PP-YOLOE/graph_surgery_compare.py \
 --model pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_embed.onnx \
---input-shape 1,3,640,640 --ep coreml --warmup 2 --runs 8 \
+--input-shape 1,3,640,640 --ep coreml --warmup 3 --runs 10 \
 --outdir pipeline/PP-YOLOE/models/surgery \
 --fix-input-shapes --rewrite-hardswish \
 --split-concat 8 \
