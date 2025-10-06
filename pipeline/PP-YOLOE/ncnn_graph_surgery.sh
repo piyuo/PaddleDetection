8dc7a0b684e106ed9ee8ae1197a97fb3b5533a07
piyuo <piyuo.com@gmail.com>
2025-10-05 20:34:30 -0700

    Fix NCNN graph surgery script and inference script to use updated model file names

--- pipeline/PP-YOLOE/ncnn_graph_surgery.sh
+++ pipeline/PP-YOLOE/ncnn_graph_surgery.sh
@@ -7,7 +7,7 @@ source pipeline/PP-YOLOE/venv/bin/activate
 
 mkdir -p pipeline/PP-YOLOE/models/ncnn
 
-python3 pipeline/PP-YOLOE/coreml_graph_surgery.py \
+python3 pipeline/PP-YOLOE/ncnn_graph_surgery.py \
 --model pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman.onnx \
 --input-shape 1,3,640,640 --warmup 20 --runs 80 \
 --img pipeline/dataset/demo/demo.jpg \
@@ -24,6 +24,6 @@ python3 pipeline/PP-YOLOE/coreml_graph_surgery.py \
 
 pnnx pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.onnx
 
-cp pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.bin pipeline/PP-YOLOE/models/
-cp pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.param pipeline/PP-YOLOE/models/
-python3 pipeline/PP-YOLOE/clean_pnnx_expression.py pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.param
+cp pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.bin pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_ncnn.bin
+cp pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.param pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_ncnn.param
+#python3 pipeline/PP-YOLOE/clean_pnnx_expression.py pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.param
