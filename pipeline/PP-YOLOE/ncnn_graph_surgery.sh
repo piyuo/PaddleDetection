5cef65871683c8779eeb69a4a041599f87e978c9
piyuo <piyuo.com@gmail.com>
2025-10-06 16:14:34 -0700

    Remove the script for verifying optimized NCNN model outputs from the PP-YOLOE pipeline. This script was responsible for testing and displaying model output information, including total layers and blobs, as well as listing output layers. Its removal may indicate a shift in testing strategy or reliance on alternative verification methods.

--- pipeline/PP-YOLOE/ncnn_graph_surgery.sh
+++ pipeline/PP-YOLOE/ncnn_graph_surgery.sh
@@ -60,5 +60,5 @@ ncnnoptimize pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.p
 ./pipeline/PP-YOLOE/ncnn_inference_image.sh
 
 
-cp pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.bin pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_ncnn.bin
-cp pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.param pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_ncnn.param
+#cp pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.bin pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_ncnn.bin
+#cp pipeline/PP-YOLOE/models/ncnn/ppyoloe_crn_s_36e_pphuman_ncnn.ncnn.param pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_ncnn.param
