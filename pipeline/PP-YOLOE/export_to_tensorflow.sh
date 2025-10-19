12135b1db01d6ede5548f4fe2050c6e254894237
piyuo <piyuo.com@gmail.com>
2025-10-18 17:52:21 -0700

    feat: use pyton 311 env

--- pipeline/PP-YOLOE/export_to_tensorflow.sh
+++ pipeline/PP-YOLOE/export_to_tensorflow.sh
@@ -3,7 +3,7 @@
 
 # Activate the Python virtual environment
 echo "🔧 Activating Python environment..."
-source pipeline/PP-YOLOE/venv/bin/activate
+source pipeline/PP-YOLOE/venv311/bin/activate
 
 python3 pipeline/PP-YOLOE/export_to_tensorflow.py \
   --model pipeline/PP-YOLOE/models/ppyoloe_crn_s_36e_pphuman_cust.onnx \
