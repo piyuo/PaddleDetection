b40603ded54dcae4450ba50ec3e71ebde9f8ae4b
piyuo <piyuo.com@gmail.com>
2025-12-03 22:01:21 -0800

    feat: update ONNX inference script to use the correct human_reid model and add a backup script for model building

--- PP-YOLOE/src/reid/onnx_inference_reid.sh
+++ PP-YOLOE/src/reid/onnx_inference_reid.sh
@@ -7,6 +7,6 @@ echo "🔧 Running ONNX Object Detection + ReID inference..."
 python3 PP-YOLOE/src/reid/onnx_inference_reid.py \
     --img PP-YOLOE/build/dataset/demo/demo.jpg \
     --det_onnx PP-YOLOE/build/models/ppyoloe_crn_s_36e_pphuman_cust_ane_cu.onnx \
-    --reid_onnx PP-YOLOE/build/models/human_reid_quant.onnx \
+    --reid_onnx PP-YOLOE/build/models/human_reid.onnx \
     --out PP-YOLOE/build/output \
     --thresh 0.5
